d7b9cfecac27f3aa5d0c2d0824f618e9a11ee839
kbpranav <kbpranav.24@gmail.com>
2024-07-31 14:33:21 +0200

    test file docstrings and config values to full scale

--- tests/run_test.py
+++ tests/run_test.py
@@ -7,34 +7,57 @@ import torch.nn as nn
 from torch.utils import data
 
 # Add the parent directory to the system path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import src.network as network
 from src.metrics import StreamSegMetrics
 from utils import load_config, process_dataset, generate_pdf
 
+
 def main(config_path):
+    """
+    Main function to run the training and evaluation pipeline.
+
+    Args:
+        config_path (str): Path to the configuration JSON file.
+
+    This function performs the following steps:
+    1. Loads the configuration from the specified JSON file.
+    2. Sets up the computing device (CPU or GPU).
+    3. Initializes random seeds for reproducibility.
+    4. Sets up the metrics for evaluation.
+    5. Iterates through different checkpoint files and processes datasets.
+    6. Saves the results in a JSON file and optionally generates a PDF.
+    """
     config = load_config(config_path)
-    os.environ['CUDA_VISIBLE_DEVICES'] = config['gpu_id']
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    os.environ["CUDA_VISIBLE_DEVICES"] = config["gpu_id"]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Device: %s" % device)
-    torch.manual_seed(config['random_seed'])
-    np.random.seed(config['random_seed'])
+    torch.manual_seed(config["random_seed"])
+    np.random.seed(config["random_seed"])
     print("Setting up metrics")
-    metrics = StreamSegMetrics(config['num_classes'])
+    metrics = StreamSegMetrics(config["num_classes"])
     results = []
-    orders = ['asc', 'desc', 'rand']
+    orders = ["asc", "desc", "rand"]
     for order in orders:
-        results.append({'order': order, 'checkpoints': []})
-    checkpoint_files = [f for f in os.listdir(config['checkpoint_dir']) if f.endswith('.pth') and any(keyword in f for keyword in orders)]
-    
+        results.append({"order": order, "checkpoints": []})
+    checkpoint_files = [
+        f
+        for f in os.listdir(config["checkpoint_dir"])
+        if f.endswith(".pth") and any(keyword in f for keyword in orders)
+    ]
+
     for checkpoint_file in checkpoint_files:
-        config['checkpoint_file'] = checkpoint_file
-        model = network.modeling.__dict__[config['model']](num_classes=config['num_classes'], output_stride=config['output_stride'])
-        print("Model: %s, Output Stride: %d" % (config['model'], config['output_stride']))
-        checkpoint_path = os.path.join(config['checkpoint_dir'], checkpoint_file)
+        config["checkpoint_file"] = checkpoint_file
+        model = network.modeling.__dict__[config["model"]](
+            num_classes=config["num_classes"], output_stride=config["output_stride"]
+        )
+        print(
+            "Model: %s, Output Stride: %d" % (config["model"], config["output_stride"])
+        )
+        checkpoint_path = os.path.join(config["checkpoint_dir"], checkpoint_file)
         if os.path.isfile(checkpoint_path):
-            checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+            checkpoint = torch.load(checkpoint_path, map_location=torch.device("cpu"))
             print("Model restored from %s" % checkpoint_path)
             if "model_state" not in checkpoint:
                 print("Key 'model_state' not found in checkpoint")
@@ -45,17 +68,24 @@ def main(config_path):
             del checkpoint
         else:
             print(f"Checkpoint file not found: {checkpoint_path}")
-        
-        process_dataset(config['json_file1'], 'cityscapes', config, model, device, metrics, results)
-        process_dataset(config['json_file2'], 'kitti_360', config, model, device, metrics, results)
 
-    pdf_file = os.path.join(config['checkpoint_dir'], 'validation_results_bn.pdf')
-    with open(os.path.join(config['checkpoint_dir'], 'validation_results_bn.json'), 'w') as file:
+        process_dataset(
+            config["json_file1"], "cityscapes", config, model, device, metrics, results
+        )
+        process_dataset(
+            config["json_file2"], "kitti_360", config, model, device, metrics, results
+        )
+
+    pdf_file = os.path.join(config["checkpoint_dir"], "validation_results_bn.pdf")
+    with open(
+        os.path.join(config["checkpoint_dir"], "validation_results_bn.json"), "w"
+    ) as file:
         json.dump(results, file, indent=4)
     # generate_pdf(results, pdf_file)
     print(f"Results saved to {pdf_file}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: main.py <path_to_config.json>")
         sys.exit(1)
--- tests/utils.py
+++ tests/utils.py
@@ -9,7 +9,23 @@ from reportlab.lib import colors
 from datasets.kitti_360 import KittiDatasetLoader
 from datasets.cityscapes_v1 import CityscapesDatasetLoader
 
+
 def validate(opts, model, loader, device, metrics, image_paths, label_paths):
+    """
+    Validate the model on the given data loader.
+
+    Args:
+        opts (dict): Configuration options.
+        model (torch.nn.Module): Model to validate.
+        loader (torch.utils.data.DataLoader): Data loader for validation data.
+        device (torch.device): Device to run the validation on (CPU or GPU).
+        metrics (StreamSegMetrics): Metrics object to update and retrieve results.
+        image_paths (list): List of image file paths.
+        label_paths (list): List of label file paths.
+
+    Returns:
+        dict: Validation scores.
+    """
     metrics.reset()
     with torch.no_grad():
         for i, (images, labels) in tqdm(enumerate(loader)):
@@ -22,106 +38,177 @@ def validate(opts, model, loader, device, metrics, image_paths, label_paths):
         score = metrics.get_results()
     return score
 
+
 def generate_pdf(results, output_file):
+    """
+    Generate a PDF report of the validation results.
+
+    Args:
+        results (list): List of results dictionaries.
+        output_file (str): Path to save the generated PDF.
+    """
     doc = SimpleDocTemplate(output_file, pagesize=landscape(letter))
     elements = []
 
-    data = [["Order", "Dataset", "Checkpoint Name", "Overall Accuracy", "Mean Accuracy", "FreqW Accu", "Mean mIoU"]]
+    data = [
+        [
+            "Order",
+            "Dataset",
+            "Checkpoint Name",
+            "Overall Accuracy",
+            "Mean Accuracy",
+            "FreqW Accu",
+            "Mean mIoU",
+        ]
+    ]
     miou_values = []
 
     for result in results:
-        order = result['order']
-        for checkpoint in result['checkpoints']:
-            metrics_val = checkpoint['metrics_val']
-            miou = metrics_val['Mean IoU']
+        order = result["order"]
+        for checkpoint in result["checkpoints"]:
+            metrics_val = checkpoint["metrics_val"]
+            miou = metrics_val["Mean IoU"]
             miou_values.append(f"{miou:.6f}")
-            data.append([
-                order,
-                checkpoint['dataset'],
-                checkpoint['name'],
-                f"{metrics_val['Overall Acc']:.6f}",
-                f"{metrics_val['Mean Acc']:.6f}",
-                f"{metrics_val['FreqW Acc']:.6f}",
-                f"{miou:.6f}"
-            ])
+            data.append(
+                [
+                    order,
+                    checkpoint["dataset"],
+                    checkpoint["name"],
+                    f"{metrics_val['Overall Acc']:.6f}",
+                    f"{metrics_val['Mean Acc']:.6f}",
+                    f"{metrics_val['FreqW Acc']:.6f}",
+                    f"{miou:.6f}",
+                ]
+            )
 
     # Find the min and max mIoU values
     min_miou = float(min(miou_values))
     max_miou = float(max(miou_values))
 
     table = Table(data)
-    table.setStyle(TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.white),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Times-Roman'),
-        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.white),
-        ('GRID', (0, 0), (-1, -1), 1, colors.black)
-    ]))
+    table.setStyle(
+        TableStyle(
+            [
+                ("BACKGROUND", (0, 0), (-1, 0), colors.white),
+                ("TEXTCOLOR", (0, 0), (-1, 0), colors.black),
+                ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                ("FONTNAME", (0, 0), (-1, 0), "Times-Roman"),
+                ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+                ("BACKGROUND", (0, 1), (-1, -1), colors.white),
+                ("GRID", (0, 0), (-1, -1), 1, colors.black),
+            ]
+        )
+    )
 
     # Apply color to min and max mIoU values
     for i in range(1, len(data)):
         miou = float(data[i][6])
         if miou == min_miou:
-            table.setStyle(TableStyle([('BACKGROUND', (6, i), (6, i), colors.red)]))
+            table.setStyle(TableStyle([("BACKGROUND", (6, i), (6, i), colors.red)]))
         elif miou == max_miou:
-            table.setStyle(TableStyle([('BACKGROUND', (6, i), (6, i), colors.green)]))
+            table.setStyle(TableStyle([("BACKGROUND", (6, i), (6, i), colors.green)]))
 
     elements.append(table)
     doc.build(elements)
 
+
 def process_dataset(json_file, dataset_name, config, model, device, metrics, results):
-    print(json_file)
-    if dataset_name == 'cityscapes':
+    """
+    Process a dataset for validation.
+
+    Args:
+        json_file (str): Path to the JSON file containing dataset information.
+        dataset_name (str): Name of the dataset ('cityscapes' or 'kitti_360').
+        config (dict): Configuration options.
+        model (torch.nn.Module): Model to validate.
+        device (torch.device): Device to run the validation on (CPU or GPU).
+        metrics (StreamSegMetrics): Metrics object to update and retrieve results.
+        results (list): List to store the validation results.
+    """
+    # print(json_file)
+    if dataset_name == "cityscapes":
         dataset_loader = CityscapesDatasetLoader()
-    elif dataset_name == 'kitti_360':
+    elif dataset_name == "kitti_360":
         dataset_loader = KittiDatasetLoader()
     else:
         raise ValueError(f"Invalid dataset: {dataset_name}")
 
-    with open(json_file, 'r') as file:
+    with open(json_file, "r") as file:
         json_data = json.load(file)
-    
+
     val_image_paths = []
     val_ground_truth_paths = []
     for item in json_data:
-        if item.get('image_exists') and item.get('ground_truth_exists'):
-            val_image_paths.append(item.get('image'))
-            val_ground_truth_paths.append(item.get('ground_truth'))
+        if item.get("image_exists") and item.get("ground_truth_exists"):
+            val_image_paths.append(item.get("image"))
+            val_ground_truth_paths.append(item.get("ground_truth"))
 
     # Randomly sample 500 images
-    if len(val_image_paths) > config['num_test']:
-        sampled_indices = np.random.choice(len(val_image_paths), config['num_test'], replace=False)
+    if len(val_image_paths) > config["num_test"]:
+        sampled_indices = np.random.choice(
+            len(val_image_paths), config["num_test"], replace=False
+        )
         val_image_paths = [val_image_paths[i] for i in sampled_indices]
         val_ground_truth_paths = [val_ground_truth_paths[i] for i in sampled_indices]
 
     print(f"Number of validation images for {dataset_name}: {len(val_image_paths)}")
-    print(f"Number of validation ground truth images for {dataset_name}: {len(val_ground_truth_paths)}")
-    
+    print(
+        f"Number of validation ground truth images for {dataset_name}: {len(val_ground_truth_paths)}"
+    )
+
     val_dst = dataset_loader.get_datasets(val_image_paths, val_ground_truth_paths)
-    val_loader = data.DataLoader(val_dst, batch_size=config['val_batch_size'], shuffle=True, num_workers=2)
-    
+    val_loader = data.DataLoader(
+        val_dst, batch_size=config["val_batch_size"], shuffle=True, num_workers=2
+    )
+
     model.eval()
-    val_score = validate(opts=config, model=model, loader=val_loader, device=device, metrics=metrics, image_paths=val_image_paths, label_paths=val_ground_truth_paths)
+    val_score = validate(
+        opts=config,
+        model=model,
+        loader=val_loader,
+        device=device,
+        metrics=metrics,
+        image_paths=val_image_paths,
+        label_paths=val_ground_truth_paths,
+    )
     print(metrics.to_str(val_score))
 
     class_names = [
-    'road', 'sidewalk', 'building', 'wall', 'fence', 'pole', 'traffic light', 'traffic sign', 
-    'vegetation', 'terrain', 'sky', 'person', 'rider', 'car', 'truck', 'bus', 'train', 
-    'motorcycle', 'bicycle']
+        "road",
+        "sidewalk",
+        "building",
+        "wall",
+        "fence",
+        "pole",
+        "traffic light",
+        "traffic sign",
+        "vegetation",
+        "terrain",
+        "sky",
+        "person",
+        "rider",
+        "car",
+        "truck",
+        "bus",
+        "train",
+        "motorcycle",
+        "bicycle",
+    ]
 
     # metrics.plot_confusion_matrix(class_names = class_names)
-    
+
     for result in results:
-        if result['order'] in config['checkpoint_file']:
-            result['checkpoints'].append({
-                "name": config['checkpoint_file'],
-                "metrics_val": val_score,
-                "dataset": dataset_name
-            })
+        if result["order"] in config["checkpoint_file"]:
+            result["checkpoints"].append(
+                {
+                    "name": config["checkpoint_file"],
+                    "metrics_val": val_score,
+                    "dataset": dataset_name,
+                }
+            )
             break
 
+
 def load_config(config_path):
-    with open(config_path, 'r') as f:
+    with open(config_path, "r") as f:
         return json.load(f)
