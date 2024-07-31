4c68229d8b28ef8cabe2d468d963581da45e16a4
kbpranav <kbpranav.24@gmail.com>
2024-07-31 13:01:21 +0200

    HPO script updated

--- hpo/hpo.py
+++ hpo/hpo.py
@@ -1,11 +1,13 @@
 import subprocess
 import json
 from datetime import datetime
-from ConfigSpace import ConfigurationSpace, UniformFloatHyperparameter, UniformIntegerHyperparameter, Configuration
+from ConfigSpace import ConfigurationSpace, UniformFloatHyperparameter, UniformIntegerHyperparameter, CategoricalHyperparameter, Configuration
 from dehb import DEHB
 import logging
 import os
 import sys
+import argparse
+
 # Add the parent directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -16,7 +18,7 @@ def setup_configuration_space():
     cs = ConfigurationSpace()
     lr = UniformFloatHyperparameter("lr", lower=1e-6, upper=1e-1, log=True)
     total_itrs = UniformFloatHyperparameter("total_itrs", lower=20, upper=2400, log=False)
-    batch_size = UniformFloatHyperparameter("batch_size", lower=4, upper=64, log = True)
+    batch_size = UniformFloatHyperparameter("batch_size", lower=4, upper=64, log=True)
     weight_decay = UniformFloatHyperparameter("weight_decay", lower=1e-6, upper=1e-1, log=True)
     crop_size = UniformIntegerHyperparameter("crop_size", lower=120, upper=370)
     cs.add_hyperparameters([lr, total_itrs, batch_size, weight_decay, crop_size])
@@ -33,9 +35,12 @@ def objective_function(config: Configuration, fidelity: float, **kwargs):
         "--lr", str(config["lr"]),
         "--batch_size", str(int(config["batch_size"])),
         "--crop_size", str(int(config["crop_size"])),
-        "--json_input", "./assets/kitti_360_00_filtered.json",
         "--weight_decay", str(config["weight_decay"]),
-        "--datetime", dt
+        "--json_input", "ranked_2k_class.json",
+        "--layer", "sl",
+        "--full", "True",
+        "--kd", "True",
+        "--pixel", "True"
     ]
 
     logging.info(f"Running pipeline command: {' '.join(pipeline_command)}")
@@ -45,16 +50,32 @@ def objective_function(config: Configuration, fidelity: float, **kwargs):
         logging.error(f"Pipeline command failed: {e}")
         return {"fitness": float("-inf"), "cost": fidelity}
 
+    # Creating unique checkpoint name
+    json_input = "ranked_2k_class.json"
+    layer = "sl"
+    full = "True"
+    kd = "True"
+    pixel = "True"
+
+    checkpoint_name = f"{json_input.split('.')[0]}_{layer.upper()}_{'full' if full=='True' else 'BBN'}"
+    if kd == "True":
+        checkpoint_name += "_KD"
+    if pixel == "True":
+        checkpoint_name += "_pixel"
+    
+    logging.info(f"Checkpoint name: {checkpoint_name}")
+
+    # Run the test command
     test_command = [
-        "python", "tests/test_v5.py",
+        "python", "./tests/test_v5.py",
         "--model", "deeplabv3plus_resnet101",
         "--gpu_id", "0",
-        "--checkpoint_dir", f'checkpoints/',
-        "--json_file1", "tests/cityscapes_val_set.json",
-        "--json_file2", "tests/kitti-360_val_set_v3.json",
-        "--num_test", "250"
+        "--checkpoint_dir", 'checkpoints/',
+        "--json_file1", "./tests/cityscapes_val_set.json",
+        "--json_file2", "./tests/kitti-360_val_set_v3.json",
+        "--new_ckpt", checkpoint_name,
+        "--num_test", "2200"
     ]
-
     logging.info(f"Running test command: {' '.join(test_command)}")
     try:
         subprocess.run(test_command, check=True)
@@ -62,7 +83,7 @@ def objective_function(config: Configuration, fidelity: float, **kwargs):
         logging.error(f"Test command failed: {e}")
         return {"fitness": float("-inf"), "cost": fidelity}
 
-    result_path = f'./checkpoints/validation_results_bn.json'
+    result_path = f'checkpoints/{checkpoint_name}.json'
     if not os.path.exists(result_path):
         logging.error(f"Result file not found: {result_path}")
         return {"fitness": float("-inf"), "cost": fidelity}
@@ -70,20 +91,22 @@ def objective_function(config: Configuration, fidelity: float, **kwargs):
     try:
         with open(result_path, 'r') as f:
             data = json.load(f)
-        mIoU = data[2]['checkpoints'][0]["metrics_val"]['Mean IoU']
+        city_mIoU = data[0]['checkpoints'][0]["metrics_val"]['Mean IoU']
+        kitti_mIoU = data[0]['checkpoints'][1]["metrics_val"]['Mean IoU']
+        average_mIoU = (city_mIoU + kitti_mIoU) / 2
     except (json.JSONDecodeError, KeyError) as e:
         logging.error(f"Error reading mIoU from result file: {e}")
         return {"fitness": float("-inf"), "cost": fidelity}
 
-    logging.info(f"Obtained mIoU: {mIoU}")
-    print("================================================================================\n\n")
-    return {"fitness": -mIoU, "cost": fidelity}
+    logging.info(f"Cityscapes mIoU: {city_mIoU}")
+    logging.info(f"Kitti mIoU: {kitti_mIoU}")
+    logging.info(f"Average mIoU: {average_mIoU}")
+
+    logging.info("================================================================================\n\n")
+    return {"fitness": -kitti_mIoU, "cost": fidelity}
 
 def run_dehb_optimizer(cs):
     dt = datetime.now().strftime("%Y%m%d-%H%M%S")
-    # incumbent_logs/dt folder 
-    # os.makedirs(f"incumbent_logs/{dt}", exist_ok=True)
-    # output_path = f"incumbent_logs/{dt}"
     dim = len(cs.get_hyperparameters())
     optimizer = DEHB(
         f=objective_function,
